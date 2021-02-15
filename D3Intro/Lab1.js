a56a2fe838c87fd00e4b6b694f6fd9a627928a26
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-15 12:30:12 -0800

    quick fix

--- D3Intro/Lab1.js
+++ D3Intro/Lab1.js
@@ -2,7 +2,19 @@
 /* eslint-disable no-undef */
 /* Example One: using d3 to style elements in the DOM */
 
+
+
 d3.select("div#example1").style("color", "green");
+
+
+/*  TODO 1: Rainbow Colors
+  Use the D3 to change the background color of the firstDiv to vividred(#FF0018)
+  Use the D3 to change the background color of the secondDiv to Deep Saffron(#FFA52C)
+  Use the D3 to change the background color of the thirdDiv to Maximum Yellow(#FFFF41)
+  Use the D3 to change the background color of the fourthDiv to Green (#008018)
+  Use the D3 to change the background color of the fifthDiv to Blue (#0000F9)
+  Use the D3 to change the background color of the lastDiv to Philippine Violet(#86007D)
+*/
 
 d3.select("div#firstDiv").style("background-color", "#FF0018");
 
@@ -15,17 +27,6 @@ d3.select("div#fourthDiv").style("background-color", "#008018");
 d3.select("div#fifthDiv").style("background-color", "#0000F9");
 
 d3.select("div#lastDiv").style("background-color", "#86007D");
-/*  TODO 1: Rainbow Colors
-  Use the D3 to change the background color of the firstDiv to vividred(#FF0018)
-  Use the D3 to change the background color of the secondDiv to Deep Saffron(#FFA52C)
-  Use the D3 to change the background color of the thirdDiv to Maximum Yellow(#FFFF41)
-  Use the D3 to change the background color of the fourthDiv to Green (#008018)
-  Use the D3 to change the background color of the fifthDiv to Blue (#0000F9)
-  Use the D3 to change the background color of the lastDiv to Philippine Violet(#86007D)
-*/
-
-// d3.select('');
-
 /* 
   Example Two: 
   Use d3 to load JSON data: Load the data from file sales.json
@@ -50,8 +51,9 @@ d3.json("../data/monthlySales.json").then((data) => {
     .data(data)
     .enter()
     .append("h2")
-    .text((obj) => obj.sales)
-    .style("color", "#000");
+    .text((obj) => `${obj.month}: $${obj.sales.toFixed(2)}`)
+    .style("color", "#000")
+    .style('background-color', 'yellow')
 });
 
 /* 
@@ -61,4 +63,7 @@ d3.json("../data/monthlySales.json").then((data) => {
   Stretch: Format the date as "Day Month date, Year"
 */
 
-d3.json("../data/distanceCovered.json");
+d3.json("../data/distanceCovered.json")
+  .then((data) => {
+    d3.select
+  })
